3a9e8fae314b172f3c5683dda5a2551648a37f73
Zebi lobognon <zebilobognon@gmail.com>
2026-04-27 08:18:55 +0000

    bfonklff

--- Apprend_py_exercice2/foncation11.py
+++ Apprend_py_exercice2/foncation11.py
@@ -1,10 +1,12 @@
 def first_unique(numbers):
    
-    for num in len(numbers):
+    for i in len(numbers):
         for num in len(numbers):
             if numbers[i] == numbers[j] :
                 cont+=1
                 mincont=cont        
             
                 
-        return None  # Retourne None si aucun élément n'est unique
+        return None
+    
+print(first_unique([1, 2, 2, 3, 3, 4]))
